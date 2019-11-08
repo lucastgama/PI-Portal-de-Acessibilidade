948de09842ba9290216dc2cd17deba9bd24d35ea
ClaudineiJr0 <clau1997ju@hotmail.com>
2019-11-08 20:38:03 -0300

    Alteraçao de Frames para Panels dentro do JDesktop Principal

---
+++ PortalDeAcessibilidade/src/portaldeacessibilidade/DesktopPrincipal.java
@@ -0,0 +1,106 @@
+package portaldeacessibilidade;
+
+import java.awt.Graphics;
+import java.awt.*;
+import javax.swing.*;
+
+public class DesktopPrincipal extends javax.swing.JFrame {
+    
+    //private BufferedImage img;
+
+    public DesktopPrincipal() {  
+        
+        initComponents();
+        
+        MenuPrincipal Tela = new MenuPrincipal(this.jDesktopPane1);
+        this.jDesktopPane1.add(Tela);
+        Tela.setVisible(true); 
+        this.setExtendedState(MAXIMIZED_BOTH);
+        this.setUndecorated(true);
+    } 
+
+    
+   
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        ImageIcon icon = new ImageIcon(getClass().getResource("/Menu/background.jpg"));
+        Image image = icon.getImage();
+        jDesktopPane1 = new javax.swing.JDesktopPane(){
+
+            public void paintComponent(Graphics g){
+                g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+            }
+
+        };
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(255, 255, 255));
+        setExtendedState(6);
+        setMaximumSize(new java.awt.Dimension(1920, 1080));
+        setMinimumSize(new java.awt.Dimension(1280, 720));
+        setSize(new java.awt.Dimension(1366, 768));
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jDesktopPane1.setBackground(new java.awt.Color(255, 255, 255));
+        jDesktopPane1.setMaximumSize(new java.awt.Dimension(1920, 1080));
+        jDesktopPane1.setMinimumSize(new java.awt.Dimension(1280, 720));
+        jDesktopPane1.setName(""); // NOI18N
+        jDesktopPane1.setPreferredSize(new java.awt.Dimension(1366, 768));
+
+        javax.swing.GroupLayout jDesktopPane1Layout = new javax.swing.GroupLayout(jDesktopPane1);
+        jDesktopPane1.setLayout(jDesktopPane1Layout);
+        jDesktopPane1Layout.setHorizontalGroup(
+            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1366, Short.MAX_VALUE)
+        );
+        jDesktopPane1Layout.setVerticalGroup(
+            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 768, Short.MAX_VALUE)
+        );
+
+        getContentPane().add(jDesktopPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+
+        setSize(new java.awt.Dimension(1386, 808));
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+   
+    public static void main(String args[]) {
+       
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(DesktopPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(DesktopPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(DesktopPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(DesktopPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new DesktopPrincipal().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JDesktopPane jDesktopPane1;
+    // End of variables declaration//GEN-END:variables
+}
